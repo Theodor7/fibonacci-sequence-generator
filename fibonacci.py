89741caf6287b7a6ad019f314f97834beaca7c06
Theodor7 <theodor.b.hansen@gmail.com>
2016-10-11 15:52:24 +0200

    Added golden ratio calculation
    Output now calculates the golden ratio from the last two terms of the
    generated sequence.
    Input text changed.

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,14 @@
-print '\nFIBONACCI SEQUENCE GENERATOR'
+print '\n\n*** FIBONACCI SEQUENCE GENERATOR ***\n\n  by Theodor Boending Hansen'
 
-num1 = input('\nEnter the number you want to start the sequence (Traditionally 0 or 1).\nFirst number (0-1000): ')
-if num1 < 0 or num1 > 1000 :
+num1 = input('\nEnter the two numbers you want to start the sequence (From -1000 to 1000).\nFirst number (Traditionally 0 or 1):\t')
+if num1 < -1000 or num1 > 1000 :
     num1 = 0
 
-num2 = input('\nEnter the second number of the sequence (Traditionally 1).\nSecond number (0-1000): ')
-if num2 < 0 or num2 > 1000 :
+num2 = input('Second number (Traditionally 1):\t')
+if num2 < -1000 or num2 > 1000 :
     num2 = 1
 
-count = input("\nHow long do you want the sequence to be? (2-100): ")
+count = input("\nHow many terms to you want in the sequence? (From 2 to 100):\nTerms in sequence:\t\t\t")
 if count < 2 or count > 100 :
     count = 10
 
@@ -21,4 +21,8 @@ output = ""
 for x in seq:
     output += str(x) + "\n"
 
-print output
+if seq[-2] != 0 :
+    golden_ratio = float(seq[-1])/seq[-2]
+else:
+    golden_ratio = '----'
+print output + '\nThe Golden Ratio calculated from last two terms (' + str(seq[-1]) + ' divided by ' + str(seq[-2]) + '):\n' + str(golden_ratio) + '\n\nReal value of The Golden Ratio:\n1.61803398875'
